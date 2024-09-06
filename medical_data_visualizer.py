f604b41ca1ddb6c3c94ceb097eb31b2549c8f6d3
Minh Tan To <74308549+TMT2005@users.noreply.github.com>
2024-09-06 19:04:10 -0400

    Update medical_data_visualizer.py

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -16,9 +16,7 @@ df['gluc'] = df['gluc'].apply(lambda x: 0 if x == 1 else 1)
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = df.melt(id_vars = 'cardio', 
-                     value_vars = ['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'], 
-                     value_name='value')
+    df_cat = df.melt(id_vars = 'cardio', value_vars = ['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'], value_name='value')
 
 
     # 6
@@ -63,16 +61,7 @@ def draw_heat_map():
     fig, ax = plt.subplots()
 
     # 15
-    sns.heatmap(data=corr, 
-                annot=True, 
-                fmt=".1f", 
-                linewidth=.5, 
-                mask=mask, 
-                annot_kws={'fontsize':6}, 
-                cbar_kws={"shrink": .7}, 
-                square=False, 
-                center=0, 
-                vmax=0.30);
+    sns.heatmap(data=corr, annot=True, fmt=".1f", linewidth=.5, mask=mask, annot_kws={'fontsize':6}, cbar_kws={"shrink": .7}, square=False, center=0, vmax=0.30);
 
 
     # 16
