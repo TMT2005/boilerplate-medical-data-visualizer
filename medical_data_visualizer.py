627d17db283ff028672c757b7e0d30e60f668520
Minh Tan To <tominhtan500@gmail.com>
2024-09-06 01:27:50 +0000

    commit

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,30 +4,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+df['overweight'] = ((df['weight'] / (df['height'] / 100) ** 2) > 25).astype(int)
 
 # 3
-
+df['cholesterol'] = df['cholesterol'].apply(lambda x: 0 if x == 1 else 1)
+df['gluc'] = df['gluc'].apply(lambda x: 0 if x == 1 else 1)
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
+    df_cat = df.melt(id_vars = 'cardio', 
+                     value_vars = ['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'], 
+                     value_name='value')
 
 
     # 6
-    df_cat = None
+    df_cat = pd.DataFrame({'total':df_cat.groupby(['cardio', 'variable'])['value'].value_counts()})\
+                                     .rename(columns={'cardio':'Cardio','variable':'Variable', 'value':'Value'})\
+                                     .reset_index()
     
 
     # 7
-
+    catplot = sns.catplot(data=df_cat, x='variable', y='total', col='cardio', kind='bar', hue='value')
 
 
     # 8
-    fig = None
+    fig = catplot.figure
+
 
 
     # 9
@@ -38,21 +44,35 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    df_heat = \
+        df[(df['ap_lo'] <= df['ap_hi']) & 
+        (df['height'] >= df['height'].quantile(0.025)) &
+        (df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) &
+        (df['weight'] <= df['weight'].quantile(0.975))]
 
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
+    mask = np.triu(np.ones_like(df_heat.corr(), dtype=bool))
 
 
 
     # 14
-    fig, ax = None
+    fig, ax = plt.subplots()
 
     # 15
-
+    sns.heatmap(data=corr, 
+                annot=True, 
+                fmt=".1f", 
+                linewidth=.5, 
+                mask=mask, 
+                annot_kws={'fontsize':6}, 
+                cbar_kws={"shrink": .7}, 
+                square=False, 
+                center=0, 
+                vmax=0.30);
 
 
     # 16
